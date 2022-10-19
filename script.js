097ee17c47948d2c6c1db1bd8a9616a4d6e27e2d
Chenxi Lin <lin.chenxi14@gmail.com>
2022-10-19 19:10:41 -0400

    Changed picture and changed background color

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let joke = document.getElementById('bad-link');
-let comment = document.getElementById('text')
+let comment = document.getElementById('text');
 
 function lameJoke(event) {
     Text.innerHTML = 'HAHA wrong link'

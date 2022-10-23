1ae3abd9a8e6b6d016762ca3839d52193814f942
Chenxi Lin <lin.chenxi14@gmail.com>
2022-10-23 12:45:38 -0400

    added dark Mode

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-let joke = document.getElementById('bad-link');
-let comment = document.getElementById('text');
+const dark = document.getElementById('dark-mode');
+const darkMode = document.body;
+const navDark = document.getElementById('nav-bar');
+const navDark2 = document.getElementById('change');
+const navDark3 = document.getElementById('change2');
+const navDark4 = document.getElementById('change3');
+const darkMain = document.getElementById('about-me');
 
-function lameJoke(event) {
-    Text.innerHTML = 'HAHA wrong link'
+
+function turnDark(event) {
+    dark.innerHTML = 'Normal Mode';
+    darkMode.style.backgroundColor = '#1e1e1e';
+    darkMode.style.color = 'white';
+    navDark.style.backgroundColor = 'black';
+    navDark.style.color = 'white';
+    navDark2.style.color = 'white';
+    navDark3.style.color = 'white';
+    navDark4.style.color = 'white';
+    darkMain.style.backgroundColor = 'black';
+    dark.onclick = normalMode
 }
+
+function normalMode(event) {
+    dark.innerHTML = 'Dark Mode';
+    darkMode.style.backgroundColor = '';
+    darkMode.style.color = '';
+    navDark.style.backgroundColor = '';
+    navDark.style.color = '';
+    navDark2.style.color = '';
+    navDark3.style.color = '';
+    navDark4.style.color = '';
+    darkMain.style.backgroundColor = '';
+    dark.onclick = turnDark;
+}
+
+dark.onclick = turnDark;
